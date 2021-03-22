f96f1d1aa08c51ed62482ec7ca400e3884502524
Gabriele Alessi <89archaon89@gmail.com>
2021-03-22 15:37:36 +0100

    Funzionamento "avanti e indietro"

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-function init() {
-
+function nextClick() {
+    var activeImg = $('.jq-active');
+    var nextImg = activeImg.next('img');
+    activeImg.removeClass('jq-active');
+    nextImg.addClass('jq-active');
 }
 
-init();
+function previousClick() {
+    var activeImg = $('.jq-active');
+    var prevImg = activeImg.prev('img');
+    activeImg.removeClass('jq-active');
+    prevImg.addClass('jq-active');
+}
+
+function init() {
+    $('.back').click(previousClick);
+    $('.forward').click(nextClick);
+}
+
+$(document).ready(init);
