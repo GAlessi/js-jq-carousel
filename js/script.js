f2dc3a711f35b4e00251759860486e5cb2279468
Gabriele Alessi <89archaon89@gmail.com>
2021-03-22 15:51:00 +0100

    Aggiunta loop tramite classi "firstImg" e "lastImg" e relativi "if"

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 function nextClick() {
     var activeImg = $('.jq-active');
-    var nextImg = activeImg.next('img');
-    activeImg.removeClass('jq-active');
-    nextImg.addClass('jq-active');
+    if (activeImg.hasClass('lastImg')) {
+        var nextImg = $('img.firstImg');
+        activeImg.removeClass('jq-active');
+        nextImg.addClass('jq-active');
+    }else {
+        var nextImg = activeImg.next('img');
+        activeImg.removeClass('jq-active');
+        nextImg.addClass('jq-active');
+    }
 }
 
 function previousClick() {
     var activeImg = $('.jq-active');
-    var prevImg = activeImg.prev('img');
-    activeImg.removeClass('jq-active');
-    prevImg.addClass('jq-active');
+    if (activeImg.hasClass('firstImg')) {
+        var prevImg = $('img.lastImg');
+        activeImg.removeClass('jq-active');
+        prevImg.addClass('jq-active');
+    }else {
+        var prevImg = activeImg.prev('img');
+        activeImg.removeClass('jq-active');
+        prevImg.addClass('jq-active');
+    }
 }
 
 function init() {
