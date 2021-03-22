b6bac3e0b1de15c206e2938f2bc3463d0a4499d1
Gabriele Alessi <89archaon89@gmail.com>
2021-03-22 15:55:56 +0100

    Aggiunta di commenti esplicatvi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+
+//funzione in caso di click "avanti"
 function nextClick() {
     var activeImg = $('.jq-active');
+
+    //verifico se sono arrivato all'ultima immagine
     if (activeImg.hasClass('lastImg')) {
+
+        //inizializzo una variabile che identifichi come immagine successiva la prima della serie
         var nextImg = $('img.firstImg');
         activeImg.removeClass('jq-active');
         nextImg.addClass('jq-active');
@@ -11,9 +17,14 @@ function nextClick() {
     }
 }
 
+//funzione in caso di click "indietro"
 function previousClick() {
     var activeImg = $('.jq-active');
+
+    //verifico se sono tornato alla prima immagine
     if (activeImg.hasClass('firstImg')) {
+
+        //inizializzo una variabile che identifichi come immagine successiva l'ultima della serie
         var prevImg = $('img.lastImg');
         activeImg.removeClass('jq-active');
         prevImg.addClass('jq-active');
@@ -24,9 +35,11 @@ function previousClick() {
     }
 }
 
+//attivo l'ascolto di "click" sui div contenenti le frecce
 function init() {
     $('.back').click(previousClick);
     $('.forward').click(nextClick);
 }
 
+//inizializzo la funzione dopo che js ha letto il file document
 $(document).ready(init);
